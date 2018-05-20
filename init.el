a89086123bc606933492251c8dae545545c85a23
Ruojun Meng <mengruojun@gmail.com>
2018-05-20 21:07:57 +0800

    test
    Acked-by: Ruojun Meng <mengruojun@gmail.com>

---
+++ init.el
@@ -0,0 +1,79 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+ (add-to-list 'package-archives
+                 '("melpa" . "http://melpa.org/packages/") t)
+
+;; general config
+(desktop-save-mode 1)
+
+
+
+(require 'neotree)
+(neotree-toggle)
+(neotree-dir "~/PycharmProjects/LearningPython")
+(global-set-key [f8] 'neotree-toggle)
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+
+(electric-pair-mode 1)
+(setq standard-indent 4)
+(setq-default indent-tabs-mode nil)
+
+;; magit start ;;
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+;; magit end;
+
+(scroll-bar-mode -1)
+(setq neo-smart-open t)
+(defun neotree-project-dir-toggle ()
+  "Open NeoTree using the project root, using find-file-in-project,
+or the current buffer directory."
+  (interactive)
+  (let ((project-dir
+         (ignore-errors
+           ;;; Pick one: projectile or find-file-in-project
+           ; (projectile-project-root)
+           (ffip-project-root)
+           ))
+        (file-name (buffer-file-name))
+        (neo-smart-open t))
+    (if (and (fboundp 'neo-global--window-exists-p)
+             (neo-global--window-exists-p))
+        (neotree-hide)
+      (progn
+        (neotree-show)
+        (if project-dir
+            (neotree-dir project-dir))
+        (if file-name
+            (neotree-find file-name))))))
+(define-key global-map (kbd "M-e") 'neotree-project-dir-toggle)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" default)))
+ '(package-selected-packages
+   (quote
+    (magit gited nlinum neotree monokai-theme color-theme-modern color-theme afternoon-theme))))
+ 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
