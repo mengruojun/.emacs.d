05e9f8477cb47c1641e41362599da89679b701ec
ruojunm <mengruojun@gmail.com>
2018-05-27 09:11:43 +0800

    add more config for python

--- init.el
+++ init.el
@@ -5,9 +5,17 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(elpy-enable)
+(setq python-shell-interpreter "jupyter"
+      python-shell-interpreter-args "console --simple-prompt"
+      python-shell-prompt-detect-failure-warning nil)
+(add-to-list 'python-shell-completion-native-disabled-interpreters
+             "jupyter")
+(setq python-shell-interpreter "python3"
+      python-shell-interpreter-args "-i")
  (add-to-list 'package-archives
                  '("melpa" . "http://melpa.org/packages/") t)
-
+(add-to-list 'load-path "~/.emacs.d/additional_el/")
 
 ;; general config
 (desktop-save-mode 1)
@@ -15,11 +23,15 @@
 
 
 
-
+(require 'find-file-in-project) 
 (require 'neotree)
 (neotree-toggle)
 (neotree-dir "~/PycharmProjects/LearningPython")
+(setq-local ffip-project-root "~/PycharmProjects/LearningPython")
+(setq-default neo-show-hidden-files t)
 (global-set-key [f8] 'neotree-toggle)
+(global-set-key [f7] 'find-file-in-project)
+(setq ffip-use-rust-fd t);; use fd instead of find for find files
 (setq projectile-switch-project-action 'neotree-projectile-action)
 
 
@@ -64,15 +76,56 @@ or the current buffer directory."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
  '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" default)))
+    ("ec0c9d1715065a594af90e19e596e737c7b2cdaa18eb1b71baf7ef696adbefb0" "0e8c264f24f11501d3f0cabcd05e5f9811213f07149e4904ed751ffdcdc44739" "72c530c9c8f3561b5ab3bf5cda948cd917de23f48d9825b7a781fe1c0d737f2f" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "cb39485fd94dabefc5f2b729b963cbd0bac9461000c57eae454131ed4954a8ac" "05d009b7979e3887c917ef6796978d1c3bbe617e6aa791db38f05be713da0ba0" default)))
+ '(fci-rule-color "#3C3D37")
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100))))
+ '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (magit gited nlinum neotree monokai-theme color-theme-modern color-theme afternoon-theme))))
+    (auto-virtualenv auto-virtualenvwrapper company-jedi ein elpy elpygen flycheck-pycheckers format-all ipython-shell-send jedi jedi-core kanban markdown-mode markdown-preview-eww markdown-preview-mode nlinum neotree monokai-theme magit gited color-theme-modern color-theme afternoon-theme)))
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0"))))
  
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
